c37926053e31755216d25050d87ff351b2ba0c3c
parthikmodi <parthik.modi10@gmail.com>
2019-07-05 10:46:53 -0400

    added deepspeech and speechmatics arguments

--- text-track-service.rb
+++ text-track-service.rb
@@ -1,13 +1,10 @@
 #!/usr/bin/ruby
-
 require 'json'
 require 'date'
 require 'time'
 require 'logger'
 require './lib/texttrack'
 require 'speech_to_text'
-require "./app"
-
 
 #log_dir = "/var/log/text-track-service"
 logger = Logger.new(STDOUT)
@@ -16,20 +13,52 @@ logger.level = Logger::INFO
 
 TextTrack.logger = logger
 
-def set_parameters(*params)
-  if(params.length == 4 || params.length == 5)
-  data = { :service => "#{params[0]}",
-           :published_file_path => "#{params[1]}",
-           :recordID => "#{params[2]}",
-           :auth_key => "#{params[3]}",
-           :status => "success",
-           :message => "successfully created json file",
-           :google_bucket_name => ("#{params[4]}" if "#{params[0]}" == "google")}.delete_if{ |k,v| v.nil?
-  }
+def set_parameters(params)
+
+  if (params[0] == "ibm" && params.length == 4)
+    data = {
+     :service => "#{params[0]}",
+     :published_file_path => "#{params[1]}",
+     :recordID => "#{params[2]}",
+     :auth_key => "#{params[3]}",
+     :status => "success",
+     :message => "successfully created json file for IBM"
+     #:google_bucket_name => ("#{params[4]}" if "#{params[0]}" == "google").delete_if{ |k,v| v.nil?}
+    }
+  elsif (params[0] == "google" && params.length == 5)
+    data = {
+     :service => "#{params[0]}",
+     :published_file_path => "#{params[1]}",
+     :recordID => "#{params[2]}",
+     :auth_key => "#{params[3]}",
+     :google_bucket_name => "#{params[4]}",
+     :status => "success",
+     :message => "successfully created json file for Google"
+    }
+  elsif (params[0] == "mozilla_deepspeech" && params.length == 4)
+    data = {
+     :service => "#{params[0]}",
+     :published_file_path => "#{params[1]}",
+     :recordID => "#{params[2]}",
+     :deepspeech_model_path => "#{params[3]}",
+     :status => "success",
+     :message => "successfully created json file for Google"
+    }
+  elsif (params[0] == "speechmatics" && params.length == 5)
+      data = {
+       :service => "#{params[0]}",
+       :published_file_path => "#{params[1]}",
+       :recordID => "#{params[2]}",
+       :userID => "#{params[3]}",
+       :auth_key => "#{params[4]}",
+       :status => "success",
+       :message => "successfully created json file for IBM"
+       #:google_bucket_name => ("#{params[4]}" if "#{params[0]}" == "google").delete_if{ |k,v| v.nil?}
+      }
   else
     data = {
       :status => "failed",
-      :message => "Wrong number of arguments"
+      :message => "Wrong service or check number of arguments.."
     }
   end
   return data
@@ -42,9 +71,19 @@ def create_json(data)
     file.puts "\"service\" : \"#{data[:service]}\","
     file.puts "\"published_file_path\" : \"#{data[:published_file_path]}\","
     file.puts "\"recordID\" : \"#{data[:recordID]}\","
-    file.puts "\"auth_key\" : \"#{data[:auth_key]}\","
-    if data[:service] == "google"
+
+    if data[:service] == "ibm"
+      file.puts "\"auth_key\" : \"#{data[:auth_key]}\","
+    elsif data[:service] == "google"
+      file.puts "\"auth_key\" : \"#{data[:auth_key]}\","
       file.puts "\"google_bucket_name\" : \"#{data[:google_bucket_name]}\","
+    elsif data[:service] == "mozilla_deepspeech"
+      file.puts "\"deepspeech_model_path\" : \"#{data[:deepspeech_model_path]}\","
+    elsif data[:service] == "speechmatics"
+      file.puts "\"auth_key\" : \"#{data[:auth_key]}\","
+      file.puts "\"userID\" : \"#{data[:userID]}\","
+    else
+      file.puts "\"auth_key\" : \"auth_key not found\,"
     end
     file.puts "\"message\" : \"#{data[:message]}\""
     file.puts "}"
@@ -65,12 +104,9 @@ end
 
 
 #main code starts from here
-if(ARGV.length == 5)
-  data = set_parameters(ARGV[0],ARGV[1],ARGV[2],ARGV[3],ARGV[4])
-elsif(ARGV.length == 4)
-  data = set_parameters(ARGV[0],ARGV[1],ARGV[2],ARGV[3])
-else
-end
+
+data = set_parameters(ARGV)
+print ARGV
 create_json(data)
 
 start_service(data[:recordID])
