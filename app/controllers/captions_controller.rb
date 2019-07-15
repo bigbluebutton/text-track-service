e9c67f8f6211303e97bafbf01b8618dc6221a1bd
Richard Alam <ritzalam@gmail.com>
2019-07-15 13:20:17 -0700

    - rename routes

--- app/controllers/captions_controller.rb
+++ app/controllers/captions_controller.rb
@@ -1,38 +1,30 @@
 
 class CaptionsController < ApplicationController
-    def index
-        puts "Hello World!"
-        $redis.lpush("foo", "bar")
-        puts $redis.llen("foo")
-    end
+  def index
+    puts "Hello World!"
+    puts $redis.llen("foo")
+  end
     
-    def service
-      record_id = params[:record_id]
+  def service
+    record_id = params[:record_id]
          
-      # TODO: Need to find how to get the key from settings.yaml
-      #props = YAML::load(File.open('settings.yaml'))
-      #puts "REDIS HOST=#{redis_host} PORT=#{redis_port} PASS=#{redis_password}"
+    # TODO: Need to find how to get the key from settings.yaml
+    #props = YAML::load(File.open('settings.yaml'))
+    #puts "REDIS HOST=#{redis_host} PORT=#{redis_port} PASS=#{redis_password}"
 
-      #redis_namespace = props["redis_list_namespace"]       
-      #   
-      $redis.lpush("recordings", record_id)
+    #redis_namespace = props["redis_list_namespace"]       
+    #   
+    $redis.lpush("recordings", record_id)
+  end
+    
+  def progress_id
+    @recordID = params[:id]
+    @caption = Caption.where(recordID: @recordID)
+    tp @caption
+  end
+    
+  private
+    def find_record_by_id
+      @captionfile = Captions.find(params[:id])
     end
-    
-    def progress
-        @captions = Caption.all
-        
-        tp @captions
-    end
-    
-    def progress_id
-        @recordID = params[:id]
-        @caption = Caption.where(recordID: @recordID)
-        tp @caption
-    end
-    
-    private
-        def find_record_by_id
-            @captionfile = Captions.find(params[:id])
-        end
-    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'captions#index'
-    
-    
+
   get '/', to: "captions#index", as: :captions_index
-    
-  get '/service/:record_id', to: "captions#service", as: :captions_service
-    
-  get '/progress', to: "captions#progress", as: :captions_progress
-  get '/progress/:id', to: "captions#progress_id", as: :captions_progress_id
+
+  get '/caption/:record_id', to: "captions#service", as: :captions_service
+
+  get '/status/:record_id', to: "captions#progress_id", as: :captions_progress_id
 end
