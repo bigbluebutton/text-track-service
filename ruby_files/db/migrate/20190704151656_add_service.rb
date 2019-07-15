6bbb9054c2bfad982c341330fbac5b0e47a32ad5
Richard Alam <ritzalam@gmail.com>
2019-07-15 11:32:57 -0700

    - remove unused files

--- ruby_files/db/migrate/20190704151656_add_service.rb
+++
@@ -1,9 +0,0 @@
-class AddService < ActiveRecord::Migration[5.2]
-  def change
-      change_table :progresses do |t|
-          t.string :service
-          
-      end
-      
-  end
-end
--- ruby_files/text-track-worker.rb
+++
@@ -1,3 +0,0 @@
-require "faktory"
-
-system("FAKTORY_PROVIDER=FAKTORY_URL FAKTORY_URL=tcp://:7832525986eee2f7@localhost:7419 bundle exec faktory-worker -r ./lib/texttrack/google_worker.rb")
