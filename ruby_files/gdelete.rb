638d68706005fc6a139e73d37eace34a6d7ab70d
SilentFlameCR <rodr0180@algonquinlive.com>
2019-08-01 11:10:14 -0400

    before-merge

---
+++ ruby_files/gdelete.rb
@@ -0,0 +1,10 @@
+require "google/cloud/storage"
+
+ENV['GOOGLE_APPLICATION_CREDENTIALS'] = '/D/innovation/text-track-service/test/bbb-accessibility-183f2b339bfb.json'
+
+storage = Google::Cloud::Storage.new project_id: "bbb-accessibility"
+bucket  = storage.bucket "bbb-accessibility"
+
+file = bucket.file "test.flac"
+
+file.delete
