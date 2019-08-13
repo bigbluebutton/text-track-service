781f6dae6ede143ab50d22eab4fa0a199bba626a
Richard Alam <ritzalam@gmail.com>
2019-08-13 10:47:54 -0700

    - rename vars

--- lib/texttrack/entry_worker.rb
+++ lib/texttrack/entry_worker.rb
@@ -8,34 +8,38 @@ module WM
     include Faktory::Job
     faktory_options retry: 5
 
-    def perform(record_id)
-      
-      TextTrack.logger.info("Processing analytics for #{record_id}")
+    def perform(params_json)
+      params = JSON.parse(params_json, :symbolize_names => true)
+
+      TextTrack.logger.info("Processing analytics for #{params[:record_id]}")
       props = YAML::load_file('settings.yaml')
-        
+
       props_keys = YAML::load_file('credentials.yaml')
-        
+
       apikey = props_keys["providers"][props["default_provider"]]["apikey"]
 
       default_provider = props["default_provider"]
       recordings_dir = props["recordings_dir"]
       captions_inbox_dir = props["captions_inbox_dir"]
-        
-      record_id = JSON.parse(record_id)
-        
-      params = {record_id: record_id["record_id"],
-        caption_locale: record_id["caption_locale"],
+
+
+
+      to_audio_params = {record_id: params[:record_id],
+        caption_locale: params[:caption_locale],
         provider: default_provider,
         auth_key: apikey,
         recordings_dir: recordings_dir,
         captions_inbox_dir: captions_inbox_dir}
-        
-      if(props["default_provider"] == "google") 
-         params.merge!(google_bucket_name: props_keys["providers"][props["default_provider"]]["bucket"]) 
+
+
+      if (props["default_provider"] == "google")
+         to_audio_params.merge!(google_bucket_name: props_keys["providers"][props["default_provider"]]["bucket"])
       elsif(props["default_provider"] == "speechmatics")
-          params.merge!(userID: props_keys["providers"][props["default_provider"]]["userID"])
+          to_audio_params.merge!(user_id: props_keys["providers"][props["default_provider"]]["user_id"])
       end
-      WM::ToAudioWorker.perform_async(params.to_json)
+
+      puts to_audio_params
+      WM::ToAudioWorker.perform_async(to_audio_params.to_json)
     end
   end
 end
