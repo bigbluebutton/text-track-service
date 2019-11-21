e54021e6ebbf12beed664487182be1035c4d257a
parthikmodi <parthik.modi10@gmail.com>
2019-11-21 10:49:56 -0800

    updated gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_203958) do
+ActiveRecord::Schema.define(version: 2019_07_09_150627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,15 +21,15 @@ ActiveRecord::Schema.define(version: 2019_11_15_203958) do
     t.string "status"
     t.string "caption_locale"
     t.string "error"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "bbb_url"
     t.string "bbb_checksum"
     t.string "kind"
     t.string "label"
     t.string "processtime"
     t.string "start_time"
-    t.string "duration"
+    t.string "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
