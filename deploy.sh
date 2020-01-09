ba9a7ccd40885b8cd84dee956a034e9f237bff23
parthikmodi <parthik.modi10@gmail.com>
2020-01-09 12:09:03 -0800

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #/usr/local/bin/docker-compose -f /var/docker/text-track-service/docker-compose.yml down -v
 sudo systemctl stop tts-docker
+sudo mv /var/docker/text-track-service/storage/* /var/recording_dir
 cd /var/docker/text-track-service
 git pull origin docker
 sudo -kS chmod -R a+rX *
