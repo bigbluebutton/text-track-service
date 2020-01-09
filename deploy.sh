57479668ead3bf1c9ab43e2df9cc30069e45b92d
parthikmodi <parthik.modi10@gmail.com>
2020-01-09 13:18:58 -0800

    changed deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #/usr/local/bin/docker-compose -f /var/docker/text-track-service/docker-compose.yml down -v
-#sudo systemctl stop tts-docker
+sudo systemctl stop tts-docker
 sudo docker-compose down
-ruby commands/move_files.rb
 cd /var/docker/text-track-service
 git pull origin docker
 sudo -kS chmod -R a+rX *
-sudo docker-compose up --build
-#sudo systemctl start tts-docker
+sudo systemctl start tts-docker
 #/usr/local/bin/docker-compose -f /var/docker/text-track-service/docker-compose.yml up --build -d
