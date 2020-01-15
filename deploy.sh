f6cdb5ef862d1d9d49519847b2485c95ae2c1f9e
SilentFlameCR <rodr0180@algonquinlive.com>
2020-01-15 11:58:39 -0500

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 #/usr/local/bin/docker-compose -f /var/docker/text-track-service/docker-compose.yml down -v
 sudo systemctl stop tts-docker
 cd /var/docker/text-track-service
-git pull origin fred
+git pull
 sudo -kS chmod -R a+rX *
+sudo -kS chmod -R a+rX tmp/*
 sudo systemctl start tts-docker
 #/usr/local/bin/docker-compose -f /var/docker/text-track-service/docker-compose.yml up --build -d
